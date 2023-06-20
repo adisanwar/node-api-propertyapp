fbfc2d7065fa9cbb98f8a4e73c9ef44a254ea754
synysterRev <69097348+synysterR3v@users.noreply.github.com>
2023-06-21 06:24:39 +0700

    masih error

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from "express";
 import db from "./config/database.js";
-import Users from "./models/UserModels.js";
+import router from "./routes/index.js";
+// import Users from "./models/UserModels.js";
+
 
 const app = express();
 
 try {
     await db.authenticate();
     console.log ("database Connected ...");
-    await Users.sync();
+    // await Users.sync();
 } catch (error) {
     console.log(error);
 }
 
-
-app.listen(5000, () => console.log('server running at port 5000'))
+app.use(express.json);
+app.use(router);
+app.listen(3000, () => console.log('server running at port 3000'));
